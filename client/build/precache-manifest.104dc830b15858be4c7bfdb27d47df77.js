5a66969cd2bab5a8248f6d5dabead593473a1c45
Zowie0122 <minjiayi22@gmail.com>
2020-09-27 16:09:52 +0900

    cleared the client sub-folder

---
+++ client/build/precache-manifest.104dc830b15858be4c7bfdb27d47df77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a83593d187b1c874442296c88ed1d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10b1e37b4389e06f5d18",
+    "url": "/static/css/main.ef81843d.chunk.css"
+  },
+  {
+    "revision": "cbe7acf1d329e78b8011",
+    "url": "/static/js/2.929136d9.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.929136d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b1e37b4389e06f5d18",
+    "url": "/static/js/main.ee3eb8e4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
